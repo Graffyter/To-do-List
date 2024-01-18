43ac14b726b0d49887706986708daea48e2550cd
graffyter <alejorpo97@gmail.com>
2024-01-17 23:32:04 -0500

    añadimos funcionalidad al boton para aumentar los episodios yhacemos que cuando llegue al total de episodios este se ponga en verde

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 import './CreateTodoButton.css'
 
+
 const defaultAnimes = [
   { text: "Death Note", totalEpisodes:36, episode:36 },
   { text: "naruto shipuden", totalEpisodes:500, episode:300 },
   { text: "Your Name", totalEpisodes:1, episode:0 },
-  { text: "Danganronpa", totalEpisodes:12, episode:12 },
+  { text: "Danganronpa", totalEpisodes:12, episode:10 },
 ];
+console.log(defaultAnimes);
 
 const actualizadoAnimes = defaultAnimes.map(anime => ({
   ...anime,
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,24 +5,31 @@ import React, { useState } from 'react';
 
 function TodoItem({text, episodes, episode}) {
   const [isChecked, setIsChecked] = useState(false);
+  const [currentEpisode, setCurrentEpisode] =useState(episode);
   
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
+  const handleAddEpisodeClick =() =>{
+    if(currentEpisode < episodes){
+      setCurrentEpisode(prevEpisode => prevEpisode + 1)
+      if(currentEpisode + 1 ===episodes){
+        setIsChecked(true)
+      }      
+    }
+  }
   
   const tachado ={
      color: isChecked ? 'green' : 'gray',
   };
-
     return (
       <li className='item'>
         <img width="25" height="25" src="https://img.icons8.com/ios-filled/25/000000/menu--v6.png" alt="menu--v6"/>
         <p className='frase' style={tachado}>{text}</p>
         <div className='episodes'>
-          <button id='addEpisode' onClick={(event) =>{
-          }}>+</button>
-          <p className='frase2' style={tachado}>{episode}/{episodes}</p>
+          <button id='addEpisode' onClick={handleAddEpisodeClick}>+</button>
+          <p className='frase2' style={tachado}>{currentEpisode}/{episodes}</p>
         </div>
         <div className='check'>
             <input type="checkbox"  checked ={isChecked} onChange={handleCheckboxChange}/> 
